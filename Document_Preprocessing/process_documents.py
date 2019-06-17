a8772408c5990dabeb0ee5e7c56be3c1cbf4e755
Andreas <rargor@gmail.com>
2019-06-17 22:01:14 +0200

    processed texts and sampled subset

--- Document_Preprocessing/process_documents.py
+++ Document_Preprocessing/process_documents.py
@@ -1,15 +1,36 @@
 import os
 import csv
 import utils
+import random
 
 source_dir = "./output"
 
+target_dir = "./sampled_texts/"
+
 by_speaker = dict()
+
+selected_texts = list()
 
 counter = 0
 for filename in os.listdir(source_dir):
     data = utils.get_csv(source_dir + "/" + filename)
-    print(data)
-    if counter >= 2:
-        break
-    counter += 1
+    for row in data:
+        curr_row = row.copy()
+        curr_row.append(filename[:-4])
+        curr_row.append("ORIGINAL")
+        curr_row.append(counter)
+        counter += 1
+        if row[1] in by_speaker:
+            by_speaker[row[1]].append(curr_row)
+        else:
+            by_speaker[row[1]] = [curr_row]
+
+for key in list(by_speaker.keys()):
+    if len(by_speaker[key]) < 40:
+        by_speaker.pop(key)
+    else:
+        selected_texts.extend(random.sample(by_speaker[key], 20))
+
+random.shuffle(selected_texts)
+
+utils.output_csv(target_dir + "everything", selected_texts)
--- Document_Preprocessing/utils.py
+++ Document_Preprocessing/utils.py
@@ -1,7 +1,7 @@
 import csv
 
 def output_csv(filename, data):
-    with open('./output/{}.csv'.format(filename), mode='w', encoding='utf-8') as curr_file:
+    with open('{}.csv'.format(filename), mode='w', encoding='utf-8') as curr_file:
         curr_writer = csv.writer(curr_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         for t in data:
             curr_writer.writerow(t)
