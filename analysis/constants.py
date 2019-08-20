75bbdda495bd2c4432cee2eb92b9ff13d3ac4aeb
Andreas <rargor@gmail.com>
2019-08-20 12:14:17 +0100

    more adjustments to constants

--- analysis/constants.py
+++ analysis/constants.py
@@ -40,14 +40,34 @@ CSV_ID = 6
 
 #LIST CONSTANTS
 OUTPUT_PATHS = [GENERAL_OUTPUT, TEXT_TYPES_OUTPUT]
-PUNCTUATION = [",", ".", "?", "!"]
+PUNCTUATION = [",", ".", "?", "!"] #partial, based on most common sentence boundary markers
+CONJUNCTIONS = ["og", "men", "eller", "at", "da", "fordi", "hvis", "når"] #partial, based on website lists
+# the following were harvested from the type-list by hand
 PURE_PRONOUNS = ["han", "hun", "jeg", "vi", "sig", "noget" "andre", "nogle", "dem", "mig", "vores", "deres", "mit", "sin"]
-CONJUNCTIONS = []
 DET_PRON_LIST = ["den", "de", "en", "det", "et"] #can't be used for simple pron-checking due to also working as DETs
 PROPER_NOUNS = ["ordføreren", "kommunerne", "borgerne", "enhedslisten", "enhedslistens", "regeringen", "regeringens",
                 "ministeren", "alternativet", "danmark", "regionerne", "vikingeskibsmuseet", "grønlands", "venstres",
-                "eus", "sverige", "norge", "kystdirektoratet", "eboks", "nemid", "ordførerens"]
-MULTI_WORD_PEOPLE = ["liberal alliance", "liberal alliances", "dansk folkeparti", "ida auken", "folketingets talerstol"]
-ABBREVIATIONS = ["kl.", ]
+                "eus", "sverige", "norge", "kystdirektoratet", "eboks", "nemid", "ordførerens", "christiansborg", 
+                "enhedslistens", "europa"]
+MW_PROPER_NOUNS = ["liberal alliance", "liberal alliances", "dansk folkeparti", "ida auken", "folketingets talerstol",
+                  "radikale venstre", "statens kunstfond", "pernille skipper", "mogens jensen", "jane heitmann", "isil"
+                  "christian", "lisbeth bech poulsen", "jens joel", "joachim b. olsen", "dansk folkepartis", "karsten hønge",
+                  "karina adsbøl"]
+# necessary to deal with cases where punctuation is used for more than delimiting sentences
+ABBREVIATIONS = ["kl.", "b.", "pct.", "..."]
 
-#mogens, jensen, jane, heitmann, isil, christian
+EXPECTED_MINIMAL_PAIR_OUTCOMES = [("3237 ORIGINAL", "3237 PRONOUN-NP-P", LEFT), #kulegravningen -> han, becomes inconsistent with later sentences
+                                  ("707 ORIGINAL", "707 PRONOUN-NP-P", LEFT), #de regler og vejledninger -> det, becomes more ambiguous but not that much
+                                  ("2015 ORIGINAL", "2015 PRONOUN-NP-O", LEFT), #på finansloven -> Ø, slight increase in confusion due to missing word
+                                  ("120 ORIGINAL", "120 PRONOUN-P-P", LEFT), #der -> hun (+verb agreement), the difference is ambiguous but RIGHT implies some unnamed actor
+                                  ("131 ORIGINAL", "131 PRONOUN-P-NP", NULL), #vi -> ordføreren, probably no agreement, simply different sentence meanings
+                                  ("3116 ORIGINAL", "3116 PRONOUN-NP-P", LEFT), #enhedslistens ordfører -> dem, large gap between pronoun and antecedent, likely less coherent
+                                  ("1926 ORIGINAL", "1926 PRONOUN-NP-P", ),
+                                  ("", "", ),
+                                  ("", "", ),
+                                  ("", "", ),
+                                  ("", "", ),
+                                  ("", "", ),
+                                  ("", "", ),
+                                  ("", "", ),
+                                  ("", "", )]
